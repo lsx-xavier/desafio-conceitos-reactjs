3747ecf0a4384bc4052ab550bde9efacf03b6c5b
Lucas Xavier <ls.xxavier@gmail.com>
2020-05-21 10:28:07 -0300

    Chalenge 3 done: Chalenge finished, but the function of remove i have doubts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [ repositories, setRepositories ] = useState( [] );
+
+  useEffect( () => {
+    api.get('repositories').then( response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
     // TODO
+    const response = await api.post('repositories', {
+      title: `Novo Repositorie ${Date.now()}`,
+      url: "https://github.com/josepholiveira",
+        title: "Desafio ReactJS",
+        techs: ["React", "Node.js"],
+
+    });
+
+    const repositorie = response.data;
+    setRepositories([...repositories, repositorie]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    // TODO (esta comentando como eu tinha feito antes!)
+    // const repositorieId = {id}
+    // const response = await api.delete(`/repositories/${id}`);
+    // const listRepositories = await api.get('repositories').then( response => {
+    //   setRepositories(response.data);
+    // });
+
+    await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter(repositorie => repositorie.id !== id));
+    
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map( repositorie => [
+          <li key={repositorie.id} >
+            {repositorie.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repositorie.id)}>
+              Remover
+            </button>
+          </li>
+        ])}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
